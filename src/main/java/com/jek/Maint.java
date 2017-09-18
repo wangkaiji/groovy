0a3f22038ad8f15da29cdb8e83b45a4e59f3a99c
wangkaiji <2498616590@qq.com>
2017-09-18 13:30:00 +0800

    make the process of java calling groovy script clearly

--- src/main/java/com/jek/Maint.java
+++ src/main/java/com/jek/Maint.java
@@ -12,17 +12,22 @@ public class Maint {
     public static void main(String args[])throws IOException,ResourceException,ScriptException{
 
 
-        String groovyRelPath = "groovy";
-        String groovyScriptName = "XmlParser.groovy";
+        String groovyRelPath = "groovy";    //对应pom中定义的输出路径
+        String groovyScriptName = "XmlParser.groovy";   //脚本文件名
         String fileName = "test.xml";
-
         String classpath = Maint.class.getClassLoader()
                 .getResource("").getPath();
+
         String[] roots = new String[]{classpath+groovyRelPath+File.separator};
         GroovyScriptEngine groovyScriptEngine = new GroovyScriptEngine(roots);
+        //groovy引擎会监视roots目录下的所有groovy脚本，一旦被修改，groovy就会被重新编译
+
+
         File xmlFile = new File(classpath+fileName);
         Binding binding = new Binding();
         binding.setVariable("xmlFile",xmlFile);
+        //binding将参数注入groovy脚本
+
         Object output = groovyScriptEngine.run(classpath+groovyRelPath+File.separator+groovyScriptName,binding);
         System.out.println((String)output);
 
